db0adb9f275ca02559d2a3ee318f577df15d2ea5
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-07-12 16:51:57 +0900

    0712 JPA FileUpload

---
+++ src/main/java/org/zerock/j2/dto/ProductDTO.java
@@ -0,0 +1,26 @@
+package org.zerock.j2.dto;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+
+  private Long pno;
+
+  private String pname;
+
+  private String pdesc;
+
+  private int price;
+
+  // 데이터베이스로 보내서 처리할 용도
+  private List<String> images;
+
+  // 등록,수정 업로드된 파일 데이터를 수집하는 용도
+  private List<MultipartFile> files;
+  
+}
