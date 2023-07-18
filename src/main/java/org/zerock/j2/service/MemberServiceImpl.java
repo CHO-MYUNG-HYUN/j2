89f40d70e2a313ff772b4961fb5bf9bb94963fbf
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-07-18 16:45:58 +0900

    0718 JPA Login Kakao

--- src/main/java/org/zerock/j2/service/MemberServiceImpl.java
+++ src/main/java/org/zerock/j2/service/MemberServiceImpl.java
@@ -8,6 +8,7 @@ import org.zerock.j2.entity.Member;
 import org.zerock.j2.repository.MemberRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -70,7 +71,22 @@ public class MemberServiceImpl implements MemberService {
             return dto;
         }
 
-        return null;
+        //데이터베이스에 존재하지 않는 이메일이라면
+        Member socialMember = Member.builder()
+                .email(email)
+                .pw(UUID.randomUUID().toString())
+                .nickname("SOCIAL_MEMBER")
+                .build();
+
+        memberRepository.save(socialMember);
+
+        MemberDTO dto = MemberDTO.builder()
+                .email(socialMember.getEmail())
+                .nickname(socialMember.getNickname())
+                .admin(socialMember.isAdmin())
+                .build();
+
+        return dto;
 
     }
 
